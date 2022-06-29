fa7c215fed03cb657c9d3b2c4190c6107567cae3
Sage Zaugg <sage@ultima.localdomain>
2022-06-28 20:32:17 -0700

    Convert over to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello SageZaugg.com!
+          Hello from sagezaugg.com!
         </p>
         <a
           className="App-link"
