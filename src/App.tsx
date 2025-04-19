6efd6a3ff63847c90a1aa225e6eaff2924c21251
Sage Zaugg <dsage777@gmail.com>
2025-04-19 13:48:43 -0700

    Decrease transition duration between pages to improve responsivness.

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const PageTransition: React.FC<{ children: React.ReactNode }> = ({
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: -20 }}
-      transition={{ duration: 0.3 }}
+      transition={{ duration: 0.125 }}
     >
       {children}
     </motion.div>
